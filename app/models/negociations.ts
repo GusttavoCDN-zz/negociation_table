26163ed6b2d78843dac67878e4ccd7b53fc6ba06
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-05-09 14:21:43 -0300

    docs : Adicionado comentario sobre utilização do readOnly

--- app/models/negociations.ts
+++ app/models/negociations.ts
@@ -9,6 +9,7 @@ export class Negociations {
   }
 
   // * modificador Readonly faz com que o dado não possa ser modificado diretamente. Em array por exemplo, não sera possivel utilizar métodos como o PoP ou Shift que alteram diretamente o array Original.
+  // ? Generics declaração verbosa = ReadonlyArray<tipoDoDado>
   list(): readonly Negociation[] {
     return this.negociations;
     // return [...this.negociations]; // * Cria uma referencia a lista para nunca alterar a lista original em JS!
